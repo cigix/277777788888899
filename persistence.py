66dc31e55b0e6414e53c36690f2762dc56009ecc
Clément Gillard <clement.gillard@trust-in-soft.com>
2019-03-22 11:57:15 +0100

    Add decomposing loop

--- persistence.py
+++ persistence.py
@@ -18,3 +18,22 @@ def decompose(n):
     if 1 < n < 10:
         res.remove(n)
     return res
+
+if __name__ == '__main__':
+    persistence = dict()
+    for i in range(10):
+        persistence[i] = 1
+    todo = list(range(1, 10))
+    most_persistent = 0
+    while todo:
+        n = todo.pop(0)
+        multiples = decompose(n)
+        for m in multiples:
+            persistence[m] = persistence[n] + 1
+            if persistence[most_persistent] < persistence[m]:
+                most_persistent = m
+            print("persistence of", m, "is", persistence[m])
+        todo = sorted(todo + multiples)
+        print(n, persistence[n], multiples, sep='\t')
+    print("most persistent", most_persistent, "with a persistence of",
+            persistence[most_persistent])
